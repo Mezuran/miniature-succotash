f86eed8c76d7a34d558bf10902b354e454c4ab32
Mezuran <mithomizuno@gmail.com>
2026-01-27 16:29:32 +0700

    Adding CI GitHub Actions

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -1,6 +1,8 @@
 import streamlit as st
 
+# Penting dikasih, setiap page
 PAGE_CONFIG = {"title": "My Dashboard", "icon": ":material/home:"}
 
+# Page body
 st.header("Dashboard")
 st.write("Welcome to the dashboard.")
--- pages/master.py
+++ pages/master.py
@@ -1,6 +1,8 @@
 import streamlit as st
 
+# Penting dikasih, setiap page
 PAGE_CONFIG = {"title": "My Master Page", "icon": ":material/home:"}
 
+# Page body
 st.header("Master Page")
 st.write("Welcome to the Page.")
