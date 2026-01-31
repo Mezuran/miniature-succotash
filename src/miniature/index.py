4a6f53d3b2c8581f9a812994edef55a698df4112
Mitho Mizuno <mithomizuno@gmail.com>
2026-02-01 00:49:03 +0700

    Fixing prisma error in streamlit share

--- src/miniature/index.py
+++ src/miniature/index.py
@@ -1,7 +1,16 @@
+import subprocess
+import sys
 import streamlit as st
 
 from pathlib import Path
 from miniature.libs.utils import get_page_config
+
+try:
+    from prisma import Prisma
+except RuntimeError:
+    print("Prisma Client not found. Generating...")
+    subprocess.run([sys.executable, "-m", "prisma", "generate"], check=True)
+    print("Prisma Client generated successfully.")
 
 st.set_page_config(layout='wide')
 
